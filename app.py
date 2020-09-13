2c0260b85f25cf07afa9b3bb85bc4df3adfae6a9
ck00lman <ckoolman1985@gmail.com>
2020-09-13 23:53:11 +0000

    Added templates folder with base.html boilerplate, static folder with css folder and style.css.

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import flask_pymongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
 
@@ -13,6 +16,12 @@ def get_recipes():
     return render_template('recipes.html', 
     recipes=mongo.db.recipes.find())
 
+# This functionality allows user to click on the Like button and it will increment the total count
+@app.route('/like/<recipe_id>', methods=["POST"])
+def like(recipe_id):
+    mongo.db.recipes.update_one({"_id": ObjectId(recipe_id)}, {"$inc":
+                                                               {'likes': 1}})
+    return redirect(url_for('recipe_complete', recipe_id=recipe_id))
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
