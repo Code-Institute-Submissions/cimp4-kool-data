a7baab00a77e818ff709bb0ea770d9948547fc82
ck00lman <ckoolman1985@gmail.com>
2020-09-13 23:15:21 +0000

    Added Environment Variables for Mongo Database and URI

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 @app.route('/')
 def hello():
